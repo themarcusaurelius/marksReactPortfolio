983837b4e67ffd7c205120ce4f6b92f7eabecba5
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-31 14:55:39 -0500

    Testing Full Stack Deployment

--- server.js
+++ server.js
@@ -11,14 +11,13 @@ connectDB();
 app.use(express.json({ extended: false }))
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'))
+app.use('/api/users', require('./routes/api/users'));
 
 //Configuration for Express to behave correctly in production environment
 if (process.env.NODE_ENV === 'production') {
     //First - Making sure express will serve production assets - main.js, main.css, etc
     app.use(express.static('client/build'));
     //Second -Express will serve up the index.html file if it doesn't recognize the route
-    const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     });
